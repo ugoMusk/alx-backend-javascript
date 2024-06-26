16d8f658e9e5eaa2a2347cf2c0b2a09f07c416eb
ugoMusk <ugobothankgod@gmail.com>
2024-06-26 18:06:18 +0100

    update babel configuration

--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,13 +1,12 @@
-
-41;320;0cmodule.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
-};
+module.exports = {
+	presets: [
+	  [
+		'@babel/preset-env',
+		{
+		  targets: {
+			node: 'current',
+		  },
+		},
+	  ],
+	],
+  };
