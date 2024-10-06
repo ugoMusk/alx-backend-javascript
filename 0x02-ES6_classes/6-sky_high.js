b993ccf96caf5e48fcb001fd3e81cc8d28c6b05f
ugoMusk <ugobothankgod@gmail.com>
2024-10-06 07:21:44 +0100

    submit solution for the 6-sky_high task

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
