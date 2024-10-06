79f90103b2839ff359b6a834540c7e510d1ba474
ugoMusk <ugobothankgod@gmail.com>
2024-10-06 07:21:55 +0100

    submit solution for the 7-airport task

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
+
+export default Airport;
